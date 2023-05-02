94105c21394bf8d546d8b5397d371fcd1b03c65c
André Costa <adcosta118@gmail.com>
2023-05-02 22:23:18 +0100

    Fix React app

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator
-          </a>
+            Fib Calculator 2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
